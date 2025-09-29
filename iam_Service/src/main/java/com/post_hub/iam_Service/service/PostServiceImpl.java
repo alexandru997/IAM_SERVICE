f713be4d8dd7f2e4239bb4ed45db3ed851fa5289
Alexandru <besliualexandru33@gmail.com>
2025-09-29 23:10:46 +0300

    DI-Constructor practice

---
+++ iam_Service/src/main/java/com/post_hub/iam_Service/service/PostServiceImpl.java
@@ -0,0 +1,17 @@
+package com.post_hub.iam_Service.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private final List<String> posts =  new ArrayList<>();
+
+    @Override
+    public void CreatePost(String postContent){
+        posts.add(postContent);
+    }
+}
