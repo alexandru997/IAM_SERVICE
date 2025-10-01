579ea85c537044dc7f2bfaa395f3eb70c7fe1eb7
Alexandru <besliualexandru33@gmail.com>
2025-10-01 22:32:42 +0300

    add Post entity with JPA annotations

---
+++ iam_Service/src/main/java/com/post_hub/iam_Service/model/enteties/Post.java
@@ -0,0 +1,26 @@
+package com.post_hub.iam_Service.model.enteties;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime create = LocalDateTime.now();
+
+    @Column(nullable = false, columnDefinition = "integer default 0")
+    private String content;
+}
