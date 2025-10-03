0c1e45b0b876972972f6debd8f57983c9ce23e31
Alexandru <besliualexandru33@gmail.com>
2025-10-03 20:56:27 +0300

    integrate MapStruct for object mapping in `PostServiceImpl`, add `PostMapper`, update dependencies and related configurations

---
+++ iam_Service/src/main/java/com/post_hub/iam_Service/mapper/PostMapper.java
@@ -0,0 +1,25 @@
+package com.post_hub.iam_Service.mapper;
+
+import com.post_hub.iam_Service.model.dto.post.PostDTO;
+import com.post_hub.iam_Service.model.enteties.Post;
+import org.hibernate.type.descriptor.DateTimeUtils;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.Objects;
+
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        imports = {DateTimeUtils .class, Objects .class}
+)
+public interface PostMapper {
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "content", target = "content")
+    @Mapping(source = "likes", target = "likes")
+    @Mapping(source = "created", target = "created", dateFormat = "yyyy-MM-dd'T'HH:mm:ss")
+    PostDTO toPostDTO(Post post);
+}
