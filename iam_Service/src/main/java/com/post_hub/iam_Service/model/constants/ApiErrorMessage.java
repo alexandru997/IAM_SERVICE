f848fbfb371ddf64508db99764dd17dd8c02df1a
Alexandru <besliualexandru33@gmail.com>
2025-10-04 15:13:02 +0300

    add validation for `PostRequest`, handle `DataExistException`, enhance `CommonControllerAdvice` with custom exception handling, and check for duplicate titles in `PostServiceImpl`

--- iam_Service/src/main/java/com/post_hub/iam_Service/model/constants/ApiErrorMessage.java
+++ iam_Service/src/main/java/com/post_hub/iam_Service/model/constants/ApiErrorMessage.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ApiErrorMessage {
-    POST_NOT_FOUND_BY_ID("Post with ID: %s not found");
+    POST_NOT_FOUND_BY_ID("Post with ID: %s not found"),
+    POST_ALREADY_EXISTS("Post with title: %s already exists")
+    ;
 
     private final String message;
 
