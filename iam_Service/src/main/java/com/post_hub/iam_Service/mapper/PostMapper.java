4173f75cfa738e2d772cbf9103415fc2d8a7cdf4
Alexandru <besliualexandru33@gmail.com>
2025-10-05 15:59:36 +0300

    change `updatePost` method to return void in `PostMapper` and remove `updated` field from `PostDTO`

--- iam_Service/src/main/java/com/post_hub/iam_Service/mapper/PostMapper.java
+++ iam_Service/src/main/java/com/post_hub/iam_Service/mapper/PostMapper.java
@@ -33,5 +33,5 @@ public interface PostMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "created", ignore = true)
-    Post updatePost(@MappingTarget Post post, UpdatePostRequest postRequest);
+    void updatePost(@MappingTarget Post post, UpdatePostRequest postRequest);
 }
