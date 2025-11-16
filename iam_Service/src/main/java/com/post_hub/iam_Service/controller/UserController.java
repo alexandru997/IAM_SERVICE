ad5fe7bda95ddff640f22922d53fd9e2335220b5
Alexandru <besliualexandru33@gmail.com>
2025-11-16 21:07:21 +0200

    add `createUser` functionality in `UserService`, `UserController`, and related components, introduce `NewUserRequest`, handle `DataExistException` for duplicate usernames and emails, and enhance `User` entity with default timestamps

--- iam_Service/src/main/java/com/post_hub/iam_Service/controller/UserController.java
+++ iam_Service/src/main/java/com/post_hub/iam_Service/controller/UserController.java
@@ -2,17 +2,16 @@ package com.post_hub.iam_Service.controller;
 
 import com.post_hub.iam_Service.model.constants.ApiLogoMessage;
 import com.post_hub.iam_Service.model.dto.user.UserDTO;
+import com.post_hub.iam_Service.model.request.user.NewUserRequest;
 import com.post_hub.iam_Service.model.response.IamResponse;
 import com.post_hub.iam_Service.service.UserService;
 import com.post_hub.iam_Service.utils.APIUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -27,6 +26,15 @@ public class UserController {
             @PathVariable(name = "id") Integer userId) {
         log.trace(ApiLogoMessage.NAME_OF_CURRENT_METHOD.getValue(), APIUtils.getMethodName());
         IamResponse<UserDTO> response = userService.getById(userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("${end.points.create}")
+    public ResponseEntity<IamResponse<UserDTO>> createUser(
+            @RequestBody @Valid NewUserRequest request) {
+        log.trace(ApiLogoMessage.NAME_OF_CURRENT_METHOD.getValue(), APIUtils.getMethodName());
+
+        IamResponse<UserDTO> response = userService.createUser(request);
         return ResponseEntity.ok(response);
     }
 }
--- iam_Service/src/main/java/com/post_hub/iam_Service/mapper/UserMapper.java
+++ iam_Service/src/main/java/com/post_hub/iam_Service/mapper/UserMapper.java
@@ -3,14 +3,22 @@ package com.post_hub.iam_Service.mapper;
 
 import com.post_hub.iam_Service.model.dto.user.UserDTO;
 import com.post_hub.iam_Service.model.enteties.User;
+import com.post_hub.iam_Service.model.enums.RegistrationStatus;
+import com.post_hub.iam_Service.model.request.user.NewUserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring",
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        imports = {RegistrationStatus.class, Object.class}
 )
 public interface UserMapper {
-    @Mapping(source="last_login", target="lastLogin")
+    @Mapping(source = "last_login", target = "lastLogin")
     UserDTO toDto(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "created", ignore = true)
+    @Mapping(target = "registrationStatus", expression = "java(RegistrationStatus.ACTIVE)")
+    User createUser(NewUserRequest newUserRequest);
 }
