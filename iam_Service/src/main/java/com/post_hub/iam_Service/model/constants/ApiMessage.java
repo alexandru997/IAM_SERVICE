a6d5f8736fdd8cddc6f516485ea3c7bb42f404a4
Alexandru <besliualexandru33@gmail.com>
2025-11-23 15:25:32 +0200

    add DTOs for `LoginRequest` and `UserProfile`, implement `ApiMessage` constants, and extend `IamResponse` with a `createSuccessfulWithNewToken` method

---
+++ iam_Service/src/main/java/com/post_hub/iam_Service/model/constants/ApiMessage.java
@@ -0,0 +1,13 @@
+package com.post_hub.iam_Service.model.constants;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ApiMessage {
+    TOKEN_CREATED_OR_UPDATED("User's token has been created or updated");
+
+    private final String message;
+}
---
+++ iam_Service/src/main/java/com/post_hub/iam_Service/model/dto/user/UserProfile.java
@@ -0,0 +1,24 @@
+package com.post_hub.iam_Service.model.dto.user;
+
+import com.post_hub.iam_Service.model.dto.role.RoleDTO;
+import com.post_hub.iam_Service.model.enums.RegistrationStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class UserProfile implements Serializable {
+    private Integer id;
+    private String username;
+    private String email;
+
+    private RegistrationStatus registrationStatus;
+    private LocalDateTime lastLogin;
+
+    private String token;
+    private List<RoleDTO> roles;
+}
