c3405b8cd89f77579c531c33299de7431a28c4ca
Alexandru <besliualexandru33@gmail.com>
2025-12-11 18:13:18 +0200

    add UserControllerTest cases: implement integration tests for `createUser_AsAdmin_OK_200`, `createUser_asUser_Unauthorized_401`, `updateUser_AsAdmin_OK_200`, `deleteUser_AsAdmin_200_OK`, and `deleteUser_AsUser_Unauthorized_401` to validate user creation, update, and deletion logic with roles; enhance `UpdateUserRequest` with Lombok annotations to streamline initialization

--- iam_Service/src/main/java/com/post_hub/iam_Service/model/request/user/UpdateUserRequest.java
+++ iam_Service/src/main/java/com/post_hub/iam_Service/model/request/user/UpdateUserRequest.java
@@ -1,11 +1,14 @@
 package com.post_hub.iam_Service.model.request.user;
 
-import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateUserRequest  implements Serializable {
 
     private String username;
